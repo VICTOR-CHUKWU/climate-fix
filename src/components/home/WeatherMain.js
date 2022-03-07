8e24f8841913373c616f12f79d007bc5d59a6ef4
victor-chukwu <okenwa1993@gmail.com>
2022-03-07 12:16:40 +0100

    Add api for weather

--- src/components/home/WeatherMain.js
+++ src/components/home/WeatherMain.js
@@ -1,44 +1,84 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-// import weather from '../../images/home/climate-home.png';
+import moment from 'moment';
+import fetchWeather from './weatherService';
 
-const WeatherMain = () => (
-  <Container
-    className="weather-main"
-    style={{
+const WeatherMain = () => {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [currentWeather, setCurrentWeather] = useState([]);
+
+  const fetchdata = async (lat, long) => {
+    const data = await fetchWeather(lat, long);
+    const dataWeather = setCurrentWeather(data);
+    return dataWeather;
+  };
+
+  useEffect(async () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+    await fetchdata(lat, long);
+  }, [lat, long]);
+
+  if (typeof currentWeather.main === 'undefined') {
+    return (
+      <h2>Loading</h2>
+    );
+  }
+  return (
+    <Container
+      className="weather-main"
+      style={{
       // eslint-disable-next-line global-require
-      backgroundImage: `url(${require('../../images/home/climate-home.png')})`,
-      backgroundPosition: 'center',
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-    }}
-  >
-    <Row className="text-center">
-      <Col xs={12} md={6}>
-        <div className="weather-center">
-          <div className="btn-circle">
-            <div className="btn-inner-circle">
-              <h3>26 deg</h3>
+        backgroundImage: `url(${require('../../images/home/climate-home.png')})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <Row className="text-center">
+        <Col xs={12} md={6}>
+          <div className="weather-center">
+            <div className="btn-circle">
+              <div className="btn-inner-circle">
+                <h3 className="text-white">
+                  {currentWeather.main.temp}
+                  {' '}
+                  &deg;C
+                </h3>
+              </div>
             </div>
           </div>
-        </div>
-      </Col>
-      <Col xs={12} md={6} className="weather-details">
-        <div className="details-inner">
-          <div className="d-flex">
-            <div className="mr-4">
-              <p className="text-white mb-0">Sunday 4th jan</p>
-              <p className="text-white mt-0">Gmt 12 noon</p>
+        </Col>
+        <Col xs={12} md={6} className="weather-details">
+          <div className="details-inner">
+            <div className="d-flex">
+              <div className="mr-4">
+                <p className="text-white mb-0">{currentWeather.name}</p>
+                <p className="text-white mt-0">
+                  {moment().format('LL')}
+                </p>
+              </div>
+              <div className="ml-3">
+                <p className="text-white mb-0">
+                  {currentWeather.main.humidity}
+                  {' '}
+                  %
+                </p>
+                <p className="text-white mt-0">chance of rain</p>
+              </div>
             </div>
-            <div className="ml-3">
-              <p className="text-white mb-0">90%</p>
-              <p className="text-white mt-0">chance of rain</p>
-            </div>
+            <h5 className="text-white text-left">
+              {currentWeather.weather[0].description}
+            </h5>
           </div>
-          <h5 className="text-white text-left">if it rains heavily it might cause problem</h5>
-        </div>
-      </Col>
-    </Row>
-  </Container>
-);
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default WeatherMain;
